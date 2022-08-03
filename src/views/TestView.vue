214044358326aac22b7b3b145f00bfa291c2a8ce
mariley <mariley2018@gmail.com>
2022-08-02 20:34:20 -0500

    add route to test svg js

---
+++ src/views/TestView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    Hola
+
+    <Brand07 />
+  </div>
+</template>
+
+<script>
+import Brand07 from '@/components/Brand07.vue'
+export default {
+  components: {
+    Brand07
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
