ecbb50f27b59b6b2c31fd43dcd33dc563effc878
mariley <mariley2018@gmail.com>
2022-08-11 16:47:02 -0500

    add constants brands.js , general.js

---
+++ src/components/constants/brands.js
@@ -0,0 +1,10 @@
+export const BRANDS = [
+  {
+    text_default: 'Hello',
+    show_percentage: false,
+    type: 'text',
+    brackground_image: '',
+    selected: false,
+    component: ''
+  }
+]
---
+++ src/components/constants/general.js
@@ -0,0 +1,9 @@
+export const GENERAL =
+  {
+    name: 'chispitass',
+    primary_color: '#F51414',
+    secundary_color: '#FF8D08',
+    language: 'en',
+    logo_url: 'https://olaclick.s3.amazonaws.com/companies/logos/3e9e3277-30e0-4c8c-8648-bc92ce29934b.png',
+    web_url: 'http://chispitas.app.localhost:3000'
+  }
